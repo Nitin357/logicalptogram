4745cba15aabf80ad3a52d475dabfb02c2342b01
Nmagar357 <92437216+Nmagar357@users.noreply.github.com>
2023-07-09 09:28:30 +0530

    Swaping two number using third variable logical program

---
+++ src/logicalprogram/SwapTwoNumbereUsingThirdVariable.java
@@ -0,0 +1,20 @@
+package logicalprogram;
+
+public class SwapTwoNumbereUsingThirdVariable {
+
+    public static void main (String [] args)
+    {
+
+        int A=5;
+        int B=6;
+        int Z;
+
+        System.out.println("Befor swaping:-"+A+" : "+B);
+        //swaping
+        Z=A;
+        A=B;
+        B=Z;
+        System.out.println("After swaping:-"+A+" : "+B);
+
+    }
+}
